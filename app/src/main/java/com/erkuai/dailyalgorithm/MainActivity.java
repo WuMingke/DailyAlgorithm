67beee3d9b64d62c3e0b1aec27abbe06b4619d1f
WuMingKe <766190553@qq.com>
2018-10-10 22:35:20 +0800

    20181010冒泡排序

--- app/src/main/java/com/erkuai/dailyalgorithm/MainActivity.java
+++ app/src/main/java/com/erkuai/dailyalgorithm/MainActivity.java
@@ -2,6 +2,9 @@ package com.erkuai.dailyalgorithm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //1.冒泡排序
+        /**
+         * 比较相邻元素
+         */
+        int[] numbers = new int[]{3,2,1,4,8,0,6};
+        int[] bubbleSort = bubbleSort(numbers);
+        Log.i("algorithm", Arrays.toString(bubbleSort));
+
+    }
+
+    private int[] bubbleSort(int[] numbers){
+        int temp = 0;
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[ j + 1 ]){
+                    temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1]  =temp;
+                }
+            }
+        }
+        return numbers;
     }
 }
