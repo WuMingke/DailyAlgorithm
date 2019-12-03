c5ae7d14b046afdbe682ce3c55b69ec3f835feb0
wumingke <766190553@qq.com>
2019-12-03 14:49:06 +0800

    leetcode_easy

---
+++ app/src/main/java/com/erkuai/dailyalgorithm/leetcode_easy/No14.java
@@ -0,0 +1,50 @@
+package com.erkuai.dailyalgorithm.leetcode_easy;
+
+public class No14 {
+
+    public static void main(String args[]) {
+        String a = "abcdef";
+        String b = "d";
+        int i = a.indexOf(b);
+        System.out.println(i);
+    }
+    //编写一个函数来查找字符串数组中的最长公共前缀。
+//
+// 如果不存在公共前缀，返回空字符串 ""。
+//
+// 示例 1:
+//
+// 输入: ["flower","flow","flight"]
+//输出: "fl"
+//
+//
+// 示例 2:
+//
+// 输入: ["dog","racecar","car"]
+//输出: ""
+//解释: 输入不存在公共前缀。
+//
+//
+// 说明:
+//
+// 所有输入只包含小写字母 a-z 。
+// Related Topics 字符串
+
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            if (strs.length == 0) return "";
+            String prefix = strs[0];
+            for (int i = 0; i < strs.length; i++) {
+                while (strs[i].indexOf(prefix) != 0) {
+                    prefix = prefix.substring(0, prefix.length() - 1);
+                    if (prefix.isEmpty()) return "";
+                }
+            }
+            return prefix;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
