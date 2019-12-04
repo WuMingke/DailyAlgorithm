444eab864700b2dccd90b07468930afc0eff68a1
wumingke <766190553@qq.com>
2019-12-04 15:20:41 +0800

    leetcode_easy

---
+++ app/src/main/java/com/erkuai/dailyalgorithm/leetcode_easy/No107.java
@@ -0,0 +1,75 @@
+package com.erkuai.dailyalgorithm.leetcode_easy;
+
+import com.erkuai.dailyalgorithm.entity.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class No107 {
+
+    //给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+//
+// 例如：
+//给定二叉树 [3,9,20,null,null,15,7],
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7
+//
+//
+// 返回其自底向上的层次遍历为：
+//
+// [
+//  [15,7],
+//  [9,20],
+//  [3]
+//]
+//
+// Related Topics 树 广度优先搜索
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> levelOrderBottom(TreeNode root) {
+            ArrayList<List<Integer>> result = new ArrayList<>();
+            LinkedList<TreeNode> treeNodes = new LinkedList<>();
+            treeNodes.add(root);
+            if (root == null) return result;
+
+            while (!treeNodes.isEmpty()) {
+                List<Integer> list = new ArrayList<>();
+                int size = treeNodes.size();
+                for (int i = 0; i < size; i++) {
+                    TreeNode treeNode = treeNodes.remove();
+                    list.add(treeNode.val);
+                    if (treeNode.left != null) {
+                        treeNodes.add(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        treeNodes.add(treeNode.right);
+                    }
+                }
+                result.add(list);
+            }
+
+            Collections.reverse(result);
+            return result;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
---
+++ app/src/main/java/com/erkuai/dailyalgorithm/leetcode_easy/No83.java
@@ -0,0 +1,47 @@
+package com.erkuai.dailyalgorithm.leetcode_easy;
+
+import com.erkuai.dailyalgorithm.entity.ListNode;
+
+public class No83 {
+
+    //给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+//
+// 示例 1:
+//
+// 输入: 1->1->2
+//输出: 1->2
+//
+//
+// 示例 2:
+//
+// 输入: 1->1->2->3->3
+//输出: 1->2->3
+// Related Topics 链表
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode(int x) { val = x; }
+     * }
+     */
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+            ListNode currentNode = head;
+            while (currentNode != null && currentNode.next != null) {
+                if (currentNode.next.val == currentNode.val) {
+                    currentNode.next = currentNode.next.next;
+                } else {
+                    currentNode = currentNode.next;
+                }
+            }
+            return head;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
