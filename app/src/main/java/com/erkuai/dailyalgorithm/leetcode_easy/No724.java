27f00210d56a48866f69c20fd209b525a9cef443
wumingke <766190553@qq.com>
2020-02-14 10:15:21 +0800

    leetcode_easy

--- app/src/main/java/com/erkuai/dailyalgorithm/leetcode_easy/No724.java
+++ app/src/main/java/com/erkuai/dailyalgorithm/leetcode_easy/No724.java
@@ -40,6 +40,13 @@ public class No724 {
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public int pivotIndex(int[] nums) {
+            int sum = 0, leftsum = 0;
+            for (int x: nums) sum += x;
+            for (int i = 0; i < nums.length; ++i) {
+                if (leftsum == sum - leftsum - nums[i]) return i;
+                leftsum += nums[i];
+            }
+            return -1;
 
         }
     }
