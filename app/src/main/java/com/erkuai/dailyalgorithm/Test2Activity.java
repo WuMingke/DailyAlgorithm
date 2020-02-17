d582400ea59387b358587ea5350141016e3db75c
wumingke <766190553@qq.com>
2020-02-17 11:50:24 +0800

    案例测试

---
+++ app/src/main/java/com/erkuai/dailyalgorithm/Test2Activity.java
@@ -0,0 +1,12 @@
+package com.erkuai.dailyalgorithm;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Test2Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
--- app/src/main/java/com/erkuai/dailyalgorithm/TestActivity.java
+++ app/src/main/java/com/erkuai/dailyalgorithm/TestActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.erkuai.dailyalgorithm.algorithm.FindNumber;
 import com.erkuai.dailyalgorithm.algorithm.PrintListReversing;
@@ -16,7 +17,15 @@ import com.erkuai.dailyalgorithm.algorithm.ReplaceBlank;
 
 public class TestActivity extends AppCompatActivity {
 
-    private static final String TAG = "ALGORITHM";
+    //    private static final String TAG = "ALGORITHM";
+    private static final String TAG = "life_test";
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart()");
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,9 +34,57 @@ public class TestActivity extends AppCompatActivity {
 
         // boolean b = FindNumber.find2(new int[][]{{1, 2, 3}, {1, 5, 6}}, 5);
 
-       // String s = ReplaceBlank.replaceBlank3("123 33");
+        // String s = ReplaceBlank.replaceBlank3("123 33");
         //Log.i(TAG, s);
 
+        Log.i(TAG, "onCreate()");
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart()");
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Log.i(TAG, "onAttachedToWindow()");
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        Log.i(TAG, "onDetachedFromWindow()");
+    }
+
+    public void showTestDialog(View view) {
+        TestDialog testDialog = new TestDialog();
+        testDialog.show(getFragmentManager(), "");
     }
 }
